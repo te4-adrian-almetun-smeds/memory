3880a5f62dbd003276e09b104e85e522f5d34623
te4-adrian-almetun-smeds <adrianalmetunsmeds@hotmail.com>
2020-09-30 20:40:03 +0200

    Game won & restart screen

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
 const img = ['10_tiger.png', '11_penguin.png', '12_racoon.png', '1_pig.png', '2_squirrel.png', '3_rabbit.png', '4_frog.png', '5_fox.png', '6_bear.png', '7_monkey.png', '8_panda.png', '9_chick.png']
-const template = document.querySelector('#card')
-const wrapper = document.querySelector('.wrapper')
+const template = qs('#card')
+const wrapper = qs('.wrapper')
 let currentlySelected = []
 let data = {}
+
+function qs(selector) {
+  return document.querySelector(selector)
+}
+
+function qsa(selector) {
+  return document.querySelectorAll(selector)
+}
 
 function createCard(id) {
   let card = template.content.cloneNode(true)
@@ -12,7 +20,7 @@ function createCard(id) {
 }
 
 function uuidv4() {
-  return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+  return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
     (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
   );
 }
@@ -26,20 +34,20 @@ function cardCanBePressed(element) {
 
 // The card has been flipped?
 // #=> true / false
-function cardIsFlipped(element){
+function cardIsFlipped(element) {
   return (element.classList.contains('flipped'))
 }
 
 // Is there a pair?
 // #=> true / false
-function isPair(element){
+function isPair(element) {
   return (data[currentlySelected[0].getAttribute('data-id')] == data[element.getAttribute('data-id')])
 }
 
 // Should the card be flipped back? There are two flipped cards
 // Ie. Is flipped and not locked
 // #=> true / false
-function shouldFlipBack(element){
+function shouldFlipBack(element) {
   return (element.classList.contains('flipped') && element.getAttribute('data-lock') != 'true')
 }
 
@@ -64,7 +72,7 @@ function flipCard(element) {
 }
 
 function cardPressed(e) {
-  if (cardCanBePressed(e.target)){    
+  if (cardCanBePressed(e.target)) {
     if (cardIsFlipped(e.target)) {
       flipBackCard(e.target)
     } else {
@@ -73,10 +81,13 @@ function cardPressed(e) {
       if (currentlySelected.length == 0) {
         // First press
         currentlySelected.push(e.target)
-      }else if(isPair(e.target)) {
+      } else if (isPair(e.target)) {
         // A pair has been found
         lockPair(e.target)
         currentlySelected = []
+        if (isTheGameOver()) {
+          displayGameOverOverlay()
+        }
         console.log('A pair!')
       } else {
         // Not a pair
@@ -91,24 +102,54 @@ function cardPressed(e) {
   }
 }
 
-function game(){
+function displayGameOverOverlay() {
+  let container = document.createElement('div')
+  container.classList.add('game-over')
+  console.log(container)
+  let temp = document.createElement('h1')
+  temp.innerHTML = 'You Won'
+  container.appendChild(temp)
+  temp = document.createElement('p')
+  temp.addEventListener('click', resetGame)
+  temp.innerHTML = 'Restart'
+  container.append(temp)
+  wrapper.appendChild(container)
+  setTimeout(() => {
+    qs('.game-over').style.right = '0';
+  }, 1500);
+}
+
+// Is the game over?
+// #=> true/false
+function isTheGameOver() {
+  return qsa('.flipped[data-lock="true"]').length == img.length * 2
+}
+
+function game() {
   let holder = []
   // Generate gameboard
   for (let e of img) {
-      let randomID = uuidv4()
-      let randomID2 = uuidv4()
-      data[randomID] = e
-      data[randomID2] = e
-      holder.push(createCard(randomID))
-      holder.push(createCard(randomID2))
+    let randomID = uuidv4()
+    let randomID2 = uuidv4()
+    data[randomID] = e
+    data[randomID2] = e
+    holder.push(createCard(randomID))
+    holder.push(createCard(randomID2))
   }
 
   // Render gameboard
-  holder.sort(function (a, b) { return 0.5 - Math.random() })
-  for(let e of holder) {
+  holder.sort(function (a, b) {
+    return 0.5 - Math.random()
+  })
+  for (let e of holder) {
     e.addEventListener('click', cardPressed)
     wrapper.appendChild(e)
   }
+}
+
+function resetGame() {
+  qs('.wrapper').innerHTML = ''
+  game()
 }
 
 game()
